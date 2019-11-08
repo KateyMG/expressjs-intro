bdae2701993941195ebfea23a0e29d9c38eae26d
Katherine Mazariegos <katherinemazariegos@ufm.edu>
2019-11-08 15:14:26 -0600

    Fuction added

--- app.js
+++ app.js
@@ -20,5 +20,17 @@ app.post('/save-user', function(req, res) {
     res.json({message:"New name added"})
 })
 
+app.post('/delete-user', function(req, res) {
+    console.log(req.body);
+    // names.pop(req.body.name); Si hago un pop saco al de arriba, no al seleccionado :(
+    for( var x = 0; x < names.length; x++){ 
+        if ( names[x] === req.body.name) {
+          names.splice(x, 1); 
+            x--;
+        }
+     }
+    res.json({message: req.body.name + "User was deleted"})
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- public/js/example.js
+++ public/js/example.js
@@ -12,18 +12,17 @@ $(document).ready(function() {
             })
         }
     })
-    $('.send').click(function() {
+    $('.delete').click(function() {
         let newName = $('#nombre').val()
-        console.log(newName);
-        if (newName && newName.length > 0) {
-            $.post('/save-user', {name:newName}, function(data, status) {
-                console.log(`${data.message} and status is ${status}`)
-                alert(data.message)
-                setTimeout(function() {
-                    location.reload();
-                }, 2000);
-            })
-        }
+        console.log("Deleted"+ newName);
+        $.post('/delete-user', {name:newName}, function(data, status) {
+            console.log(`${data.message} and status is ${status}`)
+            alert(data.message)
+            setTimeout(function() {
+                location.reload();
+            }, 2000);
+        })
+
     })
-    
 })
+
